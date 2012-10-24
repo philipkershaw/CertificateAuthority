90336324329ec907daa6e248edb92f3d81b2e8f0
Philip Kershaw <philipkershaw@users.noreply.github.com>
2012-10-24 20:41:33 +0000

    Added setup.py. setup.cfg and README files.

---
+++ setup.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+"""Distribution Utilities setup program for Certificate Authority Package
+
+Contrail Project
+"""
+__author__ = "P J Kershaw"
+__date__ = "24/10/12"
+__copyright__ = "(C) 2012 Science and Technology Facilities Council"
+__license__ = """BSD - See LICENSE file in top-level directory"""
+__contact__ = "Philip.Kershaw@stfc.ac.uk"
+__revision__ = '$Id$'
+
+# Bootstrap setuptools if necessary.
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+
+setup(
+    name =            	'ContrailCA',
+    version =         	'0.1.0',
+    description =     	'Certificate Authority',
+    long_description = 	'''\
+Provides a simple implementation of a Certificate Authority.  It uses PyOpenSSL
+for bindings to OpenSSL.  It also includes the ability to set a custom openssl
+command for more fine-grained control over the certificate issuing process.
+openssl executable must be available in the host environment for the latter.
+
+Prerequisites
+=============
+This has been developed and tested for Python 2.6 and 2.7.
+
+Installation
+============
+Installation can be performed using easy_install or pip.
+
+Configuration
+=============
+Examples are contained in ``contrail.security.ca.test``.
+''',
+    author =          	'Philip Kershaw',
+    author_email =    	'Philip.Kershaw@stfc.ac.uk',
+    maintainer =        'Philip Kershaw',
+    maintainer_email =  'Philip.Kershaw@stfc.ac.uk',
+#    url =             	'',
+    platforms =         ['POSIX', 'Linux', 'Windows'],
+    install_requires =  ['ndg_httpsclient'],
+    license =           __license__,
+    test_suite =        'contrail.security.onlineca.client.test',
+    packages =          find_packages(),
+    package_data =      {
+        'contrail.security.ca.test': [
+            'README', '*.cfg', '*.crt', '*.key',
+            'test-ca/serial', 'test-ca/index.txt', 'test-ca/newcerts/',
+            'test-ca/*.crt', 'test-ca/*.key'
+            ]
+    },
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Topic :: Security',
+        'Topic :: Internet',
+        'Topic :: Scientific/Engineering',
+        'Topic :: System :: Distributed Computing',
+        'Topic :: System :: Systems Administration :: Authentication/Directory',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    zip_safe = False
+)
